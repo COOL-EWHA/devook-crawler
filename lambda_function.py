00e2b4717747d20ddacea2dc2846bbb60af7addf
gywlsp <gywls517@gmail.com>
2021-11-24 21:58:28 +0900

    feat: select Crawler class by url in lambda_handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,25 @@
+from crawler.base import BlogCrawler
+from crawler.tistory import TistoryCrawler
 from crawler.velog import VelogCrawler
+from urllib.error import HTTPError
 
 
 def lambda_handler(event, context):
     url = event["url"]
 
-    # base_crawler = BlogCrawler(url)
-    velog_crawler = VelogCrawler(url)
-    print(velog_crawler.content)
+    base_crawler = BlogCrawler(url)
+    print(base_crawler.info)
+
+    try:
+        if "velog.io" in url:
+            crawler = VelogCrawler(url)
+        elif "tistory.com" in url:
+            crawler = TistoryCrawler(url)
+        else:
+            crawler = {"content": ""}
+
+        print(crawler.content)
+
+    except HTTPError:
+        pass
+        return
