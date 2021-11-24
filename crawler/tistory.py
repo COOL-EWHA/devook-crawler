7ed59c0be8e63dc8af6ca786390268376244c805
gywlsp <gywls517@gmail.com>
2021-11-24 21:57:49 +0900

    feat: add TistoryCrawler

---
+++ crawler/tistory.py
@@ -0,0 +1,18 @@
+from utils import remove_escape
+from crawler.base import BlogCrawler
+
+
+class TistoryCrawler(BlogCrawler):
+    def __init__(self, url):
+        BlogCrawler.__init__(self, url)
+
+    @property
+    def content(self):
+        return remove_escape(
+            (
+                self._soup.select_one("div.entry-content")
+                or self._soup.select_one("div.contents_style")
+                or self._soup.select_one("div.article_view")
+                or self._soup.select_one("article")
+            ).get_text()
+        )
