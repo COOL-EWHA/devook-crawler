973e6bb5c17544157723918479bb6a7425df3aef
gywlsp <gywls517@gmail.com>
2021-12-09 02:16:44 +0900

    chore: add test URL to test.py

--- test.py
+++ test.py
@@ -1,21 +1,24 @@
 from lambda_function import lambda_handler
 
 
-lambda_handler(
-    {
-        # "url": "https://velog.io/@edie_ko/lighthouse-performance",
-        # "url": "https://velog.io/@dell_mond/Lighthouse-%EC%82%AC%EC%9A%A9%EB%B2%95"
-        # "url": "https://velog.io/@0307kwon/%EB%8B%B9%EC%8B%A0%EC%9D%98-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8%EB%8A%94-%EC%95%88%EB%85%95%ED%95%98%EC%8B%A0%EA%B0%80%EC%9A%94"
-        # "url": "https://velog.io/@devstone/react-hook-form%EC%9C%BC%EB%A1%9C-form-%EC%B5%9C%EC%A0%81%ED%99%94%ED%95%98%EA%B8%B0"
-        # "url": "https://pronist.tistory.com/146"
-        # "url": "https://helloinyong.tistory.com/320"
-        # "url": "https://jojoldu.tistory.com/603"
-        # "url": "https://coffeeandcakeandnewjeong.tistory.com/98"
-        # "url": "https://mong-blog.tistory.com/entry/promise%EC%99%80-async-await%EC%9D%98-%EC%B0%A8%EC%9D%B4%EC%A0%90"
-        # "url": "https://jungpaeng.tistory.com/100"
-        # "url": "https://weekwith.tistory.com/entry/React%EC%9D%98-%EB%AA%A8%EB%93%A0-%EA%B2%83-01-DOM%EA%B3%BC-Virtual-DOM-%EA%B7%B8%EB%A6%AC%EA%B3%A0-CDN"
-        # "url": "https://medium.com/uniquegood/%EC%86%8C%EA%B0%9C-dc78312c51d3"
-        "url": "https://medium.com/musinsa-tech/mab-211d14d2090b"
-    },
-    None,
+print(
+    lambda_handler(
+        {
+            # "url": "https://velog.io/@edie_ko/lighthouse-performance",
+            # "url": "https://velog.io/@dell_mond/Lighthouse-%EC%82%AC%EC%9A%A9%EB%B2%95"
+            # "url": "https://velog.io/@0307kwon/%EB%8B%B9%EC%8B%A0%EC%9D%98-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8%EB%8A%94-%EC%95%88%EB%85%95%ED%95%98%EC%8B%A0%EA%B0%80%EC%9A%94"
+            # "url": "https://velog.io/@devstone/react-hook-form%EC%9C%BC%EB%A1%9C-form-%EC%B5%9C%EC%A0%81%ED%99%94%ED%95%98%EA%B8%B0"
+            # "url": "https://pronist.tistory.com/146"
+            # "url": "https://helloinyong.tistory.com/320"
+            # "url": "https://jojoldu.tistory.com/603"
+            # "url": "https://coffeeandcakeandnewjeong.tistory.com/98"
+            # "url": "https://mong-blog.tistory.com/entry/promise%EC%99%80-async-await%EC%9D%98-%EC%B0%A8%EC%9D%B4%EC%A0%90"
+            # "url": "https://jungpaeng.tistory.com/100"
+            # "url": "https://weekwith.tistory.com/entry/React%EC%9D%98-%EB%AA%A8%EB%93%A0-%EA%B2%83-01-DOM%EA%B3%BC-Virtual-DOM-%EA%B7%B8%EB%A6%AC%EA%B3%A0-CDN"
+            # "url": "https://medium.com/uniquegood/%EC%86%8C%EA%B0%9C-dc78312c51d3"
+            # "url": "https://medium.com/musinsa-tech/mab-211d14d2090b"
+            "url": "https://gywlsp.github.io/boj/1715/"
+        },
+        None,
+    )
 )
