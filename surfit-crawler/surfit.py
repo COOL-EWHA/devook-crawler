0e5ff72ea3dfd3100e2f604bd3fc0bd341bf2ebc
sw-develop <psw00719@naver.com>
2022-01-30 19:25:48 +0900

    Refactoring: 추상화 적용 - 기능별 함수로 분리 - 함수명에서 각 기능을 유추할 수 있도록 네이밍

--- surfit-crawler/surfit.py
+++ surfit-crawler/surfit.py
@@ -1,88 +1,84 @@
 """
 Surfit Crawler
+
+1. get post list page html from Surfit
+2. parse url, title, description from html
+3. insert url, title, description, category into mysql
 """
 import time
 import MySQLdb
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+surfit_url_list = ["https://www.surfit.io/explore/develop/general-dev",
+                   "https://www.surfit.io/explore/develop/web-dev",
+                   "https://www.surfit.io/explore/develop/javascript",
+                   "https://www.surfit.io/explore/develop/react",
+                   "https://www.surfit.io/explore/develop/vuejs",
+                   "https://www.surfit.io/explore/develop/angular",
+                   "https://www.surfit.io/explore/develop/nodejs",
+                   "https://www.surfit.io/explore/develop/java",
+                   "https://www.surfit.io/explore/develop/python",
+                   "https://www.surfit.io/explore/develop/php",
+                   "https://www.surfit.io/explore/develop/infra-structure",
+                   "https://www.surfit.io/explore/develop/database",
+                   "https://www.surfit.io/explore/develop/android",
+                   "https://www.surfit.io/explore/develop/ios",
+                   "https://www.surfit.io/explore/develop/git",
+                   "https://www.surfit.io/explore/develop/bigdata-ai-ml"]
 
-def main():
-    """
-    1. get post list page html from Surfit
-    2. parse url, title, description from html
-    3. insert url, title, description, category into mysql
-    """
+category_list = ["일반 개발", "웹 개발", "Javascript", "React",
+                 "Vue.js", "Angular", "Node.js", "Java",
+                 "Python", "PHP", "Infra Structure", "Database",
+                 "Android", "iOS", "Git", "빅데이터/AI/머신러닝"]
+
+
+def set_chrome_driver():
     driver = webdriver.Chrome("./chromedriver")
     driver.implicitly_wait(3)
+    return driver
 
-    surfit_url_list = ["https://www.surfit.io/explore/develop/general-dev",
-                       "https://www.surfit.io/explore/develop/web-dev",
-                       "https://www.surfit.io/explore/develop/javascript",
-                       "https://www.surfit.io/explore/develop/react",
-                       "https://www.surfit.io/explore/develop/vuejs",
-                       "https://www.surfit.io/explore/develop/angular",
-                       "https://www.surfit.io/explore/develop/nodejs",
-                       "https://www.surfit.io/explore/develop/java",
-                       "https://www.surfit.io/explore/develop/python",
-                       "https://www.surfit.io/explore/develop/php",
-                       "https://www.surfit.io/explore/develop/infra-structure",
-                       "https://www.surfit.io/explore/develop/database",
-                       "https://www.surfit.io/explore/develop/android",
-                       "https://www.surfit.io/explore/develop/ios",
-                       "https://www.surfit.io/explore/develop/git",
-                       "https://www.surfit.io/explore/develop/bigdata-ai-ml"]
 
-    category_list = [
-        "일반 개발", "웹 개발", "Javascript", "React",
-        "Vue.js", "Angular", "Node.js", "Java",
-        "Python", "PHP", "Infra Structure", "Database",
-        "Android", "iOS", "Git", "빅데이터/AI/머신러닝"]
+def loads_a_web_page_in_the_current_session(driver, url):
+    driver.get(url)
 
-    url = []
-    title = []
-    description = []
-    category = []
+
+def web_page_scroll(driver):
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+
+def set_beautifulsoup(driver):
+    html = driver.page_source
+    return BeautifulSoup(html, 'html.parser')
+
+
+def parse_url_title_description(soup, tag_name, class_name, urls, titles, descriptions, category, index):
+    items = soup.find_all(tag_name, class_name)
+
+    for item in items:
+        urls.append(item.find("div", "ct-title").contents[0].attrs['href'])
+        titles.append(item.find("div", "ct-title").text)
+        descriptions.append(item.find("div", "ct-text").text)
+        category.append(category_list[index])
+
+
+def append_data(urls, titles, descriptions, category):
     items = []
-    for i, surfit_url in enumerate(surfit_url_list):
-        driver.get(surfit_url)  # 현재 브라우저 세션에서 웹 페이지 로드
-
-        # 웹페이지 스크롤
-        last_height = driver.execute_script("return document.body.scrollHeight")
-
-        while True:
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(3)
-            new_height = driver.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                break
-            last_height = new_height
-
-        # BeaufitulSoup 적용
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-
-        # url, title, description, category 데이터 저장
-        posts_b = soup.find_all("div", "ct-item base")
-        posts_t = soup.find_all("div", "ct-item text")
-
-        for item in posts_b:
-            url.append(item.find("div", "ct-title").contents[0].attrs['href'])
-            title.append(item.find("div", "ct-title").text)
-            description.append(item.find("div", "ct-text").text)
-            category.append(category_list[i])
-
-        for item in posts_t:
-            url.append(item.find("div", "ct-title").contents[0].attrs['href'])
-            title.append(item.find("div", "ct-title").text)
-            description.append(item.find("div", "ct-text").text)
-            category.append(category_list[i])
-
-    for i, url in enumerate(url):
-        data = (url, title[i], description[i], category[i])
+    for i, url in enumerate(urls):
+        data = (url, titles[i], descriptions[i], category[i])
         items.append(data)
+    return items
 
-    # MySQL
+
+def insert_into_database(items):
     conn = MySQLdb.connect(
         user="root",
         passwd="cool",
@@ -108,8 +104,26 @@ def main():
     conn.commit()
     conn.close()
 
+
+def surfit_crawl():
+    driver = set_chrome_driver()
+
+    urls = []
+    titles = []
+    descriptions = []
+    category = []
+    for i, surfit_url in enumerate(surfit_url_list):
+        loads_a_web_page_in_the_current_session(driver, surfit_url)
+        web_page_scroll(driver)
+        soup = set_beautifulsoup(driver)
+        parse_url_title_description(soup, "div", "ct-item base", urls, titles, descriptions, category, i)
+        parse_url_title_description(soup, "div", "ct-item text", urls, titles, descriptions, category, i)
+
+    items = append_data(urls, titles, descriptions, category)
+    insert_into_database(items)
+
     driver.close()
 
 
 if __name__ == '__main__':
-    main()
+    surfit_crawl()
