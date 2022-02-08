abd5c922ecb2a5e041977103923b9ee9f75aa85a
sw-develop <psw00719@naver.com>
2022-02-08 13:51:35 +0900

    Add: 크롤링한 데이터를 csv 파일에 저장하는 기능 추가 - 카테고리 별 [()..] list(tuple) 형태의 데이터를 csv 파일에 저장 - pylint와 black formatter를 적용해 PEP8 코드 스타일에 맞게 수정

--- surfit_crawler/parsers.py
+++ surfit_crawler/parsers.py
@@ -5,7 +5,7 @@ def parse_url_title_description(soup, tag_name, class_name, posts, index):
     items = soup.find_all(tag_name, class_name)
 
     for item in items:
-        posts[0].append(item.find("div", "ct-title").contents[0].attrs['href'])  # url
+        posts[0].append(item.find("div", "ct-title").contents[0].attrs["href"])  # url
         posts[1].append(item.find("div", "ct-title").text)  # title
         posts[2].append(item.find("div", "ct-text").text)  # description
         posts[3].append(category_list[index])  # category
--- surfit_crawler/surfit.py
+++ surfit_crawler/surfit.py
@@ -6,14 +6,18 @@ Surfit Crawler
 3. insert url, title, description, category into mysql
 """
 from surfit_crawler.set_up_data import surfit_url_list
-from surfit_crawler.utils import set_chrome_driver, \
-    append_data, \
-    insert_into_database, \
-    initialize_jagged_list, \
-    one_cycle_of_crawling
+from surfit_crawler.utils import (
+    set_chrome_driver,
+    append_data,
+    insert_into_database,
+    initialize_jagged_list,
+    one_cycle_of_crawling,
+    save_to_csv,
+)
+from surfit_crawler.set_up_data import category_list
 
 
-def surfit_crawler():
+def surfit_crawler_with_db():
     driver = set_chrome_driver()
 
     posts = []
@@ -22,11 +26,37 @@ def surfit_crawler():
     for i, surfit_url in enumerate(surfit_url_list):
         one_cycle_of_crawling(driver, surfit_url, posts, i)
 
-    items = append_data(urls=posts[0], titles=posts[1], descriptions=posts[2], category=posts[3])
-    insert_into_database(items=items, user="root", passwd="cool", host="127.0.0.1", db="devook_db")
+    items = append_data(
+        urls=posts[0], titles=posts[1], descriptions=posts[2], category=posts[3]
+    )
+    insert_into_database(
+        items=items, user="root", passwd="cool", host="127.0.0.1", db="test_db"
+    )
 
     driver.quit()
 
 
-if __name__ == '__main__':
-    surfit_crawler()
+def surfit_crawler_with_csv():
+    driver = set_chrome_driver()
+
+    posts = []
+    initialize_jagged_list(list_name=posts, list_length=4)
+
+    # 과정: 크롤링 -> items에 저장 -> csv 파일에 저장
+    column = ["url", "title", "description", "category"]
+    for i, surfit_url in enumerate(surfit_url_list):
+        one_cycle_of_crawling(driver, surfit_url, posts, i)
+        items = append_data(
+            urls=posts[0], titles=posts[1], descriptions=posts[2], category=posts[3]
+        )
+        posts = [[], [], [], []]  # 리스트 초기화
+
+        save_to_csv(
+            file_name=f"csv_files/{category_list[i]}.csv", column=column, data=items
+        )
+
+    driver.quit()
+
+
+if __name__ == "__main__":
+    surfit_crawler_with_csv()
