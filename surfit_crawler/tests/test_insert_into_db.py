f24581beb7beb5f3ece7634e8e503d41f4179f8f
sw-develop <psw00719@naver.com>
2022-02-08 13:48:32 +0900

    Refactoring: 단위 테스트 코드 - black formatter 적용해 PEP8 코드 스타일에 맞게 수정 - given, when, then 패턴 추가

--- surfit_crawler/tests/test_insert_into_db.py
+++ surfit_crawler/tests/test_insert_into_db.py
@@ -1,10 +1,15 @@
 from unittest import TestCase
 
 from surfit_crawler.parsers import parse_url_title_description
-from surfit_crawler.utils import set_chrome_driver, \
-    initialize_jagged_list, \
-    convert_driver_to_beautifulsoup, \
-    append_data, insert_into_database, connect_db, disconnect_db
+from surfit_crawler.utils import (
+    set_chrome_driver,
+    initialize_jagged_list,
+    convert_driver_to_beautifulsoup,
+    append_data,
+    insert_into_database,
+    connect_db,
+    disconnect_db,
+)
 
 
 class InsertIntoDBTestCase(TestCase):
@@ -21,6 +26,7 @@ class InsertIntoDBTestCase(TestCase):
         """
         성공 : 파싱한 데이터의 개수와 데이터베이스에 저장된 개수가 동일한 경우
         """
+        # given
         url = "https://www.surfit.io/explore/develop/ios"
         index = 13
 
@@ -29,13 +35,25 @@ class InsertIntoDBTestCase(TestCase):
 
         expected = len(soup.find_all("div", "ct-item base"))
 
-        parse_url_title_description(soup=soup, tag_name="div", class_name="ct-item base", posts=self.posts, index=index)
-        items = append_data(urls=self.posts[0], titles=self.posts[1], descriptions=self.posts[2],
-                            category=self.posts[3])
-        insert_into_database(items=items, user="root", passwd="cool", host="127.0.0.1", db="test_db")
-
+        parse_url_title_description(
+            soup=soup,
+            tag_name="div",
+            class_name="ct-item base",
+            posts=self.posts,
+            index=index,
+        )
+        items = append_data(
+            urls=self.posts[0],
+            titles=self.posts[1],
+            descriptions=self.posts[2],
+            category=self.posts[3],
+        )
+        # when
+        insert_into_database(
+            items=items, user="root", passwd="cool", host="127.0.0.1", db="test_db"
+        )
         sql = "SELECT * FROM post;"
         result = self.conn.cursor().execute(sql)
         disconnect_db(self.conn)
-
+        # then
         self.assertEqual(expected, result)
